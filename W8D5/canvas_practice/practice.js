8b4659a923442483bea6cc38adf3f3e576a5e09f
Kevin Chan <chankevin13@gmail.com>
2023-03-03 08:59:54 -0500

    hw

--- W8D5/canvas_practice/practice.js
+++ W8D5/canvas_practice/practice.js
@@ -4,14 +4,14 @@ document.addEventListener("DOMContentLoaded", function(){
   canvasEl.height = 500;
 
   const ctx = canvasEl.getContext("2d");
-  ctx.fillStyle = "purple";
+  ctx.fillStyle = "red";
   ctx.fillRect(0, 0, 500, 500);
 
   ctx.beginPath();
   ctx.arc(100, 100, 20, 0, 2*Math.PI, true);
-  ctx.strokeStyle = "green";
+  ctx.strokeStyle = "blue";
   ctx.lineWidth = 5;
   ctx.stroke();
-  ctx.fillStyle = "blue";
+  ctx.fillStyle = "gray";
   ctx.fill();
 });
