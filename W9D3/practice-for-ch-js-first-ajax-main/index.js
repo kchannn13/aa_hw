9586bbb9e9b2ed9b738f75f2a79b9989ed5eeb5b
Kevin Chan <chankevin13@gmail.com>
2023-03-08 08:55:25 -0500

    edit

--- W9D3/practice-for-ch-js-first-ajax-main/index.js
+++ W9D3/practice-for-ch-js-first-ajax-main/index.js
@@ -1,11 +1,11 @@
 console.log("Hello from the console!");
 
 // Your code here
-fetch('https://api.openweathermap.org/data/2.5/weather?',
-        {header: {
-            "q": "London,UK",
-            "appid": "bcb83c4b54aee8418983c2aff3073b3b"
-        }})
+fetch("https://api.openweathermap.org/data/2.5/weather?q=London,UK&appid=bcb83c4b54aee8418983c2aff3073b3b")
+        // {header: {
+        //     "q": "London,UK",
+        //     "appid": "bcb83c4b54aee8418983c2aff3073b3b"
+        // }})
     .then(response => {
         if (response.ok){
             return response.json();
