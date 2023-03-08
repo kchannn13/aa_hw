a3d30f3c287d4d99c3c725b6185126365669d3f4
Kevin Chan <chankevin13@gmail.com>
2023-03-08 02:49:26 -0500

    w9D3

---
+++ W9D3/practice-for-ch-js-first-ajax-main/index.js
@@ -0,0 +1,22 @@
+console.log("Hello from the console!");
+
+// Your code here
+fetch('https://api.openweathermap.org/data/2.5/weather?',
+        {header: {
+            "q": "London,UK",
+            "appid": "bcb83c4b54aee8418983c2aff3073b3b"
+        }})
+    .then(response => {
+        if (response.ok){
+            return response.json();
+        } else {
+            throw response;
+        }
+    })
+    .then(console.log("Success"))
+    .catch(error => console.error(error));
+
+
+
+
+    console.log("The AJAX request has been dispatched.")
